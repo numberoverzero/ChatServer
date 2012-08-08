4376f8be96c544028e65c7bc28166470922355b6
Joe Cross <joe.mcross@gmail.com>
2012-08-07 20:01:17 -0700

    Initial Commit
    Pushing existing code

---
+++ ChatServer/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Concurrent;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Net;
+using System.Net.Sockets;
+
+using Engine.DataStructures;
+using Engine.Networking;
+using Engine.Utility;
+
+namespace ChatServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1) return;
+            var portString = args[0];
+            string logfile = null;
+            if (args.Length > 1)
+                logfile = args[1];
+            int port = int.Parse(portString);
+            var server = new ChatServer(IPAddress.Any, port, logfile);
+            server.Start();
+        }
+    }
+
+    public class ChatServer : BasicServer
+    {
+        BidirectionalDict<Client, string> nickTable;
+
+        public ChatServer(IPAddress localaddr, int port, string logfile) : base(localaddr, port, logfile)
+        {
+            nickTable = new BidirectionalDict<Client, string>();
+            OnDisconnect += Handle_OnDisconnect;
+            OnConnect += DefaultHandle_OnConnect;
+            OnConnect += Authenticate_OnConnect;
+        }
+
+        protected void Authenticate_OnConnect(object sender, ServerEventArgs args)
+        {
+            var client = args.Client;
+            try
+            {
+                Authenticate(client);
+                var nick = nickTable[client];
+                log.Info("Server:Connect:AssociatedNick:Data:Nick:<{0}>".format(nick));
+                SendMsg("{0} has joined the chat.".Timestamped().format(nick));
+            }
+            catch { }
+        }
+
+        protected void Handle_OnDisconnect(object sender, ServerEventArgs args)
+        {
+            var client = args.Client;
+            string ip = client.IPString;
+            string nick = null;
+            try
+            {
+                nick = nickTable[client];
+                nickTable.Remove(client);
+                log.Info("Server:Disconnect:DisassociatedNick:Data:Nick:<{0}>".format(nick));
+                SendMsg("{0} has left the chat.".Timestamped().format(nick));
+            }
+            catch (KeyNotFoundException)
+            {
+                log.Warn("Exception:ServerException:UnknownNickAssociation:Data:Key:<{0}>".format(ip));
+                log.Warn("Exception:ServerException:UnknownNickAssociation:Data:PossibleValue:<{0}>".format(nick));
+                log.Warn("Exception:ServerException:UnknownNickAssociation:Data:Note:User may not have authenticated with the server before disconnecting.".format(nick));
+            }
+        }
+
+        bool IsNickAvailable(string nick) { return !(String.IsNullOrEmpty(nick) || nickTable.HasItem(nick)); }
+
+        public override void Authenticate(Client client, ServerEventArgs e = null)
+        {
+            string nick = null;
+            const string nickInUseKey = "Server:AuthFail:Reason:NickInUse:Data:Nick";
+            if (e == null) e = new ServerEventArgs(false, client);
+            
+            // Need a local success variable in case base Authentication changes e.Success from true
+            var localSuccess = false;
+
+            while (true)
+            {
+                WriteMsg("Please enter a nickname:", client);
+                while (!client.HasQueuedReadMessages) Thread.Sleep(1);
+                
+                nick = client.Read();
+                if (IsNickAvailable(nick))
+                {
+                    localSuccess = e.Success = true;
+                    nickTable[client] = nick;
+
+                    WriteMsg("Successfully registered nickname: {0}".format(nick), client);
+
+                    // Replace failed attempt message with success
+                    e.Parameters.Remove(nickInUseKey);
+                    e.Parameters["Server:AuthSucceed:Data:Nick"] = nick;
+                }
+                else
+                {
+                    WriteMsg("Sorry, that name is not available.", client);
+                    e.Parameters[nickInUseKey] = nick;
+                    log.Info(nickInUseKey + ":<{0}>".format(nick));
+                }
+                
+                base.Authenticate(client, e);
+                if (localSuccess) break;
+            }
+        }
+
+        public override void ReceiveMsg(string msg, Client client)
+        {
+            var nick = nickTable[client];
+            if(String.IsNullOrEmpty(msg)) return;
+            SendMsg("{0}: {1}".format(nick, msg));
+        }
+    }
+}
